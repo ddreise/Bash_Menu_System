d374502a124ffe992ff82809a5bb36e2a0f213a2
ddreise <ddreise@gmail.com>
2019-10-07 16:14:51 -0400

    Tidying up

--- Menu_System.sh
+++ Menu_System.sh
@@ -1,15 +1,18 @@
 #!/usr/local/Cellar/bash/5.0.11/bin/bash
 #
+# Operating Systems - Assignment 2
+# Menu System
+#
+# Purpose: Menu system for manipulating files
+#
+# By: Daniel Dreise
+# All Rights Reserved 
+#
 # October 5, 2019
 
-# 2 -> the menu script must prompt the end user for a destination folder name
-# The script will attempt to create this folder, and if successful, copy the files
-# in the current folder to the new folder.
-
-# 3 -> the menu script must prompt the end user for a ZIP archive filename.
-# the script will then attempt to create a compreseed archive to that filename.
 
 
+# Output main menu
 echo 
 echo "*************** Menu System ***************"
 echo 
@@ -23,83 +26,83 @@ echo
 echo "*******************************************"
 echo
 
-# NOTES
-# Use <command &> to run process in background
 
+# Loop continues until user Exits program
 while [ "$usr_input" != "6" ]; do
 
-    read usr_input
+    read usr_input                          # wait for user input
 
-    case $usr_input in
+    case $usr_input in      
         1)  echo "List File"
             echo
-            ls -Clha                        # -C Force multi-column output; -lh Human-readable size; -a All
+            ls -Clha                        # Output file information: -C Force multi-column output; -lh Human-readable size; -a All
             echo
-            ;;
+            ;;                              # break
 
         2)  echo "Create backup"
-            echo -n "Desintation directory: "     
+            echo -n "Desintation directory: "               
             read -r des_directory                           # CANNOT USE "~" to significy home directory. Must use complete filepath
-            if [ ! -e "$des_directory" ]
+                                                            # Waits for user to input filepath
+
+            if [ ! -e "$des_directory" ]                    # Checks to see if input filepath is actually a directory/file
             then
-                echo "$des_directory is not a directory"
+                echo "$des_directory is not a directory"    # if not, output that it's not a directory/filename
             else
-                cd ~/test                                       # for testing purposes
-                src_directory=$(pwd)
-                echo "Source directory:         $src_directory"
-                echo "Destination directory:    $des_directory"
-                cp -pR "$src_directory"/* "$des_directory"
+                #cd ~/test                                          # for testing purposes
+                src_directory=$(pwd)                                # set current directory filepath to variable src_directory
+                echo "Source directory:         $src_directory"     # output source directroy
+                echo "Destination directory:    $des_directory"     # output destination directory
+                cp -pR "$src_directory"/* "$des_directory"          # copy files to new directory: -R recursively; -p keep attributes
             fi
             ;;
             
         3)  echo "Create ZIP archive"           
             echo -n "Destination directory to zip to: "
-            read -r des_zip
+            read -r des_zip                                     # Wait for user input for destination directory
             echo -n "Desired name of new zipped file: "
-            read -r des_zip_filename
+            read -r des_zip_filename                            # Wait for user input for destination filename
 
-            des_zip="$des_zip$des_zip_filename"
+            des_zip="$des_zip$des_zip_filename"                 # Append filename to destination directory
 
-            cd ~/test
-            src_zip=$(pwd)
-            echo "Source directory:         $src_zip"
-            echo "Destination directory:    $des_zip"
-
+            #cd ~/test
+            src_zip=$(pwd)                                      # Set current working directory to src_zip
+            echo "Source directory:         $src_zip"           
  
-            zip "$des_zip" -r "$src_zip"/*
+            zip "$des_zip" -r "$src_zip"/*                      # ZIP files in current directory to chosen directory with chosen filename
 
             ;;
 
         4)  echo "Output text files"
-            cd ~/test
-            for f in *.txt
+            #cd ~/test
+            for f in *.txt              # Loop through all files with .txt filetype
             do
-                less $f
+                less $f                 # Output text file to screen
                 echo
             done
 
             ;;
 
         5)  echo "Delete files"
-            cd ~/des_test
-            cur_dir=$(pwd)
-            echo "Are you sure you want to delete all files in $cur_dir? [y/n]"
-            read choice
-            if [ $choice = 'y' ]
+            #cd ~/des_test
+            cur_dir=$(pwd)                                                          # Set current directory to variable cur_dir
+            echo "Are you sure you want to delete all files in $cur_dir? [y/n]"     # Confirm deletion choice
+            read choice                                                             
+            if [ $choice = 'y' ]                            # If yes, delete all files in current directory
             then
                 rm -v $cur_dir/*
 
-            elif [ $choice = 'n' ]
+            elif [ $choice = 'n' ]                          # If no, abort delete command
             then 
                 echo "Aborting \"delete files\" command"
 
             else
-                echo "Invalid input"
+                echo "Invalid input"                        # If invalid input, return to main menu
             fi
+            echo
             ;;
 
-        6)  echo "Exit";;
-        *)  echo "Invalid input."
+        6)  echo "Exit";;                   # Exit program
+        *)  echo "Invalid input."           # If invalid input, output invalid input.
             echo
             ;;
 
